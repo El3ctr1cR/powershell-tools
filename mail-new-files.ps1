71d6e8a3e23438fcae4cf8d4df7a40b2c3a70d3c
Ruben Ruitenberg <36376474+El3ctr1cR@users.noreply.github.com>
2022-01-05 12:21:53 +0100

    Add files via upload

---
+++ mail-new-files.ps1
@@ -0,0 +1,28 @@
+Param (
+	[string]$Path = "",
+    [string]$PathOud = "",
+	[string]$SMTPServer = "",
+	[string]$From = "",
+	[string]$To = "",
+	[string]$Subject = ""
+	)
+
+[array]$attachments = Get-ChildItem $Path *.pdf | Where { $_.LastWriteTime -ge [datetime]::Now.AddMinutes(-15) }
+Start-Sleep -Seconds 3
+
+$SMTPMessage = @{
+    To = $To
+    From = $From
+	Subject = "$Subject at $Path"
+    Smtpserver = $SMTPServer
+    Attachments = $attachments.fullname
+}
+
+$File = Get-ChildItem $Path *.pdf | Where { $_.LastWriteTime -ge [datetime]::Now.AddMinutes(-15) }
+If ($File)
+{
+    $SMTPBody = ""
+	Send-MailMessage @SMTPMessage -Body $SMTPBody
+    Start-Sleep -Seconds 4
+    $File | ForEach { Move-Item -Path "$($_.FullName)" -Destination $PathOud } -Verbose
+}
